880ccd08f5d6b4f1423af968cb6f46cf0f4a58d6
Hardik_Narigra <hardiknarigra2004@gmail.com>
2026-07-03 16:08:34 +0530

    Update 05_brand.sql

--- database/oltp/schema/05_brand.sql
+++ database/oltp/schema/05_brand.sql
@@ -1,1 +1,36 @@
+/*
+==========================================================
+RetailNova Decision Intelligence Platform
+OLTP Database Schema
 
+Table      : brand
+Description: Stores product brands.
+
+Author     : Hardik Narigra
+Database   : PostgreSQL 17
+==========================================================
+*/
+
+CREATE TABLE brand
+(
+    brand_id BIGINT GENERATED ALWAYS AS IDENTITY,
+
+    brand_name VARCHAR(100) NOT NULL,
+
+    description VARCHAR(500),
+
+    is_active BOOLEAN NOT NULL
+        DEFAULT TRUE,
+
+    created_at TIMESTAMPTZ NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
+
+    updated_at TIMESTAMPTZ NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT pk_brand
+        PRIMARY KEY (brand_id),
+
+    CONSTRAINT uq_brand_name
+        UNIQUE (brand_name)
+);
