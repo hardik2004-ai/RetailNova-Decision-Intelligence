dfdd514e0442e8a0b740b2b21e618b0f9056f1f7
Hardik_Narigra <hardiknarigra2004@gmail.com>
2026-07-03 16:15:33 +0530

    Update 08_location.sql

--- database/oltp/schema/08_location.sql
+++ database/oltp/schema/08_location.sql
@@ -1,1 +1,48 @@
+/*
+==========================================================
+RetailNova Decision Intelligence Platform
+OLTP Database Schema
 
+Table      : location
+Description: Stores retail stores and warehouses.
+
+Author     : Hardik Narigra
+Database   : PostgreSQL 17
+==========================================================
+*/
+
+CREATE TABLE location
+(
+    location_id BIGINT GENERATED ALWAYS AS IDENTITY,
+
+    location_name VARCHAR(150) NOT NULL,
+
+    location_type location_type NOT NULL,
+
+    address VARCHAR(255) NOT NULL,
+
+    city VARCHAR(100) NOT NULL,
+
+    state VARCHAR(100) NOT NULL,
+
+    country VARCHAR(100) NOT NULL,
+
+    postal_code VARCHAR(20) NOT NULL,
+
+    phone_number VARCHAR(20),
+
+    is_active BOOLEAN NOT NULL
+        DEFAULT TRUE,
+
+    created_at TIMESTAMPTZ NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
+
+    updated_at TIMESTAMPTZ NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT pk_location
+        PRIMARY KEY (location_id),
+
+    CONSTRAINT uq_location_name
+        UNIQUE (location_name)
+);
