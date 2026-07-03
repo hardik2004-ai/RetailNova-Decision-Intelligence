e7e7b870605363cf48dab15e84f4363ab224240b
Hardik_Narigra <hardiknarigra2004@gmail.com>
2026-07-03 16:21:33 +0530

    Update 12_payment.sql

--- database/oltp/schema/12_payment.sql
+++ database/oltp/schema/12_payment.sql
@@ -1,1 +1,48 @@
+/*
+==========================================================
+RetailNova Decision Intelligence Platform
+OLTP Database Schema
 
+Table      : payment
+Description: Stores payment details for customer orders.
+
+Author     : Hardik Narigra
+Database   : PostgreSQL 17
+==========================================================
+*/
+
+CREATE TABLE payment
+(
+    payment_id BIGINT GENERATED ALWAYS AS IDENTITY,
+
+    order_id BIGINT NOT NULL,
+
+    payment_method VARCHAR(50) NOT NULL,
+
+    payment_status payment_status_type NOT NULL
+        DEFAULT 'PENDING',
+
+    payment_date TIMESTAMPTZ,
+
+    amount NUMERIC(12,2) NOT NULL,
+
+    transaction_reference VARCHAR(100),
+
+    created_at TIMESTAMPTZ NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
+
+    updated_at TIMESTAMPTZ NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT pk_payment
+        PRIMARY KEY (payment_id),
+
+    CONSTRAINT uq_payment_order
+        UNIQUE (order_id),
+
+    CONSTRAINT uq_payment_transaction_reference
+        UNIQUE (transaction_reference),
+
+    CONSTRAINT ck_payment_amount
+        CHECK (amount >= 0)
+);
