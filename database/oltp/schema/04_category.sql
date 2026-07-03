d1bb2adbb7a365c808fa0e0d68a05a7761a55947
Hardik_Narigra <hardiknarigra2004@gmail.com>
2026-07-03 16:07:34 +0530

    Update 04_category.sql

--- database/oltp/schema/04_category.sql
+++ database/oltp/schema/04_category.sql
@@ -1,1 +1,36 @@
+/*
+==========================================================
+RetailNova Decision Intelligence Platform
+OLTP Database Schema
 
+Table      : category
+Description: Stores product categories.
+
+Author     : Hardik Narigra
+Database   : PostgreSQL 17
+==========================================================
+*/
+
+CREATE TABLE category
+(
+    category_id BIGINT GENERATED ALWAYS AS IDENTITY,
+
+    category_name VARCHAR(100) NOT NULL,
+
+    description VARCHAR(500),
+
+    is_active BOOLEAN NOT NULL
+        DEFAULT TRUE,
+
+    created_at TIMESTAMPTZ NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
+
+    updated_at TIMESTAMPTZ NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT pk_category
+        PRIMARY KEY (category_id),
+
+    CONSTRAINT uq_category_name
+        UNIQUE (category_name)
+);
