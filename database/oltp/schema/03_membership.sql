cbd5be7e17ce5efc05d10a7a5ae7662c993a2327
Hardik_Narigra <hardiknarigra2004@gmail.com>
2026-07-03 16:04:36 +0530

    Update 03_membership.sql

--- database/oltp/schema/03_membership.sql
+++ database/oltp/schema/03_membership.sql
@@ -1,1 +1,47 @@
+/*
+==========================================================
+RetailNova Decision Intelligence Platform
+OLTP Database Schema
 
+Table      : membership
+Description: Stores RetailNova Premium membership details
+             for customers.
+
+Author     : Hardik Narigra
+Database   : PostgreSQL 17
+==========================================================
+*/
+
+CREATE TABLE membership
+(
+    membership_id BIGINT GENERATED ALWAYS AS IDENTITY,
+
+    customer_id BIGINT NOT NULL,
+
+    membership_number VARCHAR(30) NOT NULL,
+
+    status membership_status_type NOT NULL
+        DEFAULT 'ACTIVE',
+
+    start_date DATE NOT NULL,
+
+    end_date DATE,
+
+    created_at TIMESTAMPTZ NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
+
+    updated_at TIMESTAMPTZ NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT pk_membership
+        PRIMARY KEY (membership_id),
+
+    CONSTRAINT uq_membership_number
+        UNIQUE (membership_number),
+
+    CONSTRAINT ck_membership_dates
+        CHECK (
+            end_date IS NULL
+            OR end_date >= start_date
+        )
+);
