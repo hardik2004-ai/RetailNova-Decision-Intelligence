b225c6015074c4e96d1ac6f08def246a7fa8ecbf
Hardik_Narigra <hardiknarigra2004@gmail.com>
2026-07-03 17:08:32 +0530

    Update update_updated_at.sql

--- database/oltp/functions/update_updated_at.sql
+++ database/oltp/functions/update_updated_at.sql
@@ -1,1 +1,28 @@
+/*
+==========================================================
+RetailNova Decision Intelligence Platform
+OLTP Database Functions
 
+File        : update_updated_at.sql
+Description : Reusable trigger function to automatically
+              update the updated_at timestamp.
+
+Author      : Hardik Narigra
+Database    : PostgreSQL 17
+==========================================================
+*/
+
+-- ======================================================
+-- Function: update_updated_at()
+-- ======================================================
+
+CREATE OR REPLACE FUNCTION update_updated_at()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$;
