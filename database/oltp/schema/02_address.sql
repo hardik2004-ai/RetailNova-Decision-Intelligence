fde76162c06935e6526bd73068028f6d7b3880ae
Hardik_Narigra <hardiknarigra2004@gmail.com>
2026-07-03 16:02:46 +0530

    Update 02_address.sql

--- database/oltp/schema/02_address.sql
+++ database/oltp/schema/02_address.sql
@@ -1,1 +1,36 @@
 
+/*
+==========================================================
+RetailNova Decision Intelligence Platform
+OLTP Database Schema
+
+Table      : address
+Description: Stores customer address information.
+
+Author     : Hardik Narigra
+Database   : PostgreSQL 17
+==========================================================
+*/
+
+CREATE TABLE address
+(
+    address_id BIGINT GENERATED ALWAYS AS IDENTITY,
+
+    customer_id BIGINT NOT NULL,
+
+    address_line_1 VARCHAR(150) NOT NULL,
+    address_line_2 VARCHAR(150),
+
+    city VARCHAR(100) NOT NULL,
+    state VARCHAR(100) NOT NULL,
+    postal_code VARCHAR(20) NOT NULL,
+    country VARCHAR(100) NOT NULL,
+
+    is_default BOOLEAN NOT NULL DEFAULT FALSE,
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT pk_address
+        PRIMARY KEY (address_id)
+);
